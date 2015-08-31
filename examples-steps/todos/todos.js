59295c9d97fce0ebdf338954f9321e3e4fe2601f
zhangaz1 <zhangaz1@hotmail.com>
2015-08-31 14:01:48 +0800

    sort the list

--- examples-steps/todos/todos.js
+++ examples-steps/todos/todos.js
@@ -4,7 +4,11 @@ if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
         tasks: function() {
-            return Tasks.find({});
+            return Tasks.find({}, {
+                sort: {
+                    createdAt: -1
+                }
+            });
         }
     });
 
