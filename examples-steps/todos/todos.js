9ae4ce7af9a86d364228c5922815ed7a54c2a2fc
zhangaz1 <zhangaz1@hotmail.com>
2015-08-31 13:49:52 +0800

    add task by ui

--- examples-steps/todos/todos.js
+++ examples-steps/todos/todos.js
@@ -7,6 +7,20 @@ if (Meteor.isClient) {
             return Tasks.find({});
         }
     });
+
+    Template.body.events({
+        'submit .new-task': function(event) {
+            event.preventDefault();
+
+            var text = event.target.text.value;
+            Tasks.insert({
+                text: text,
+                createdAt: new Date()
+            });
+
+            event.target.text.value = '';
+        }
+    });
 }
 
 if (Meteor.isServer) {
